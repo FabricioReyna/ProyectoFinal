b63ad658de1489e5765bb15e2954b5963d5f645d
Frontend_boantek <fabricioreynacordes@gmail.com>
2022-11-21 15:36:03 -0300

    Reciente

--- js/Productos.js
+++ js/Productos.js
@@ -1,10 +1,15 @@
 const items = document.getElementById("itemsContainer")
 const templatecard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment();
+const agregar = document.getElementById("agregar")
+var et = []
 
 document.addEventListener('DOMContentLoaded', () => {
     FanARTS();
-
+    if(localStorage.getItem("fan-arts")){
+    FanARTS = JSON.parse(localStorage.getItem("fan-arts")) 
+    pintarCards()
+    }
 })
 function guardarStorage(){
     localStorage.setItem("productos", FanARTS )
@@ -33,5 +38,40 @@ const pintarCards = data =>{
         fragment.appendChild(clone)
     })
     items.appendChild(fragment)
+    localStorage.setItem("fan-arts", JSON.stringify(data))
 }
 console.log(items)
+
+
+
+agregar.addEventListener("click", () => {
+   var text = document.getElementById("text").value;
+   var textarea = document.getElementById("floatingTextarea2").value
+   var imagen = document.getElementById("formFile").value
+    et.push(
+        {
+
+            "titulo": text ,
+            "Descripcion": textarea,
+            "Layer": "Ultima actualizacion hace 0 min",
+            "img": imagen
+        }
+    );
+
+    var card =   ` <div class="card-body mb-3 cards" style="max-width: 540px;" id="template-card">
+    <div class="col-2 mb-2 col-md-3 p-3">
+      <div class="">
+        <img src="${imagen}" class="img-fluid" alt="Fan-art" id="img" style="min-height: 350px;">
+      </div>
+      <div class="card" style="min-height: 300px;">
+        <div class="card-body">
+          <h5 class="card-title">${text}</h5>
+          <p class="card-text font-bold" id="card-text2">${textarea}</p>
+          <p class="card-text"><small class="text-muted" id="text-muted">Ultima actualizacion hace 0 min</small></p>
+        </div>
+      </div>
+    </div>
+  </div>`
+  $("#items").append(card);
+
+})
